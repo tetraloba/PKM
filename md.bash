612d3d772d0bcce32344ecc03621aa6108c65348
tetraloba <four.leaves.siva7342@gmail.com>
2023-06-18 11:08:49 +0900

    Ver.0.3.0

--- md.bash
+++ md.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 fileExt='.td' # 結構使うし、変更の可能性もあるので一応変数化。置換でも良さげだけど。
 DIR=~/Notepad/PKM/
+userName='tetraloba'
 if [ $# -eq 0 -o "$1" = "mk" ]; then # mk(make)コマンド
   DATE=`date "+%Y/%m/%d %H:%M:%S"` # 現在時刻を取得
   fileName=`date "+%y%m%d%H%M%S" -d "$DATE"` # ファイル名を生成
@@ -8,8 +9,8 @@ if [ $# -eq 0 -o "$1" = "mk" ]; then # mk(make)コマンド
   echo -e "\\\title: \n\\\date: $DATE\n\\\tags: \n\\\main:" > $DIR1 # ファイルの生成とテンプレートの入力
   code -g $DIR1:1:9 # codeでtitleにカーソルを合わせて開く
   exit 0;
-fi
-if [ "$1" = "ls" ]; then # ls(list)コマンド tdファイル一覧をタイトル付きで表示する。
+
+elif [ "$1" = "ls" ]; then # ls(list)コマンド tdファイル一覧をタイトル付きで表示する。
   IFS_ORG=$IFS # IFS(区切り文字)変数のオリジナルを退避
   IFS=$'\n' # IFS変数を改行のみに変更
   arr_string=$(grep -r -A2 "^\\\title:" ${DIR}*${fileExt}) # grepの検索結果を配列に代入(改行区切り)
@@ -18,6 +19,7 @@ if [ "$1" = "ls" ]; then # ls(list)コマンド tdファイル一覧をタイト
   done
   IFS=$IFS_ORG # IFS変数を復元
   exit 0;
+
 elif [ "$1" = "find" ]; then # findコマンド
   if [ $# -lt 2 ]; then
     echo "find: too few argument";
@@ -25,20 +27,25 @@ elif [ "$1" = "find" ]; then # findコマンド
   fi
   IFS_ORG=$IFS # IFS(区切り文字)変数のオリジナルを退避
   IFS=$'\n' # IFS変数を改行のみに変更
-  arr_string=$(grep -r $2 ${DIR}*${fileExt}) # grepの検索結果を配列に代入(改行区切り)
-  for str in ${arr_string[@]}; do
-    echo ${str#$DIR} # ディレクトリ部分を取り除いて出力
+  arr_files=$(grep -lr $2 ${DIR}*${fileExt}) # grepの検索結果(ファイル名)を配列に代入(改行区切り)
+  for file_finded in ${arr_files[@]}; do
+    echo "-----${file_finded#$DIR}-----";
+    echo "$(grep -A1 "^\\\title:" ${file_finded})";
+    # grep -n --color=auto $2 ${file_finded};
+    arr_string=$(grep -n --color=always $2 ${file_finded}) # grepの検索結果を配列に代入(改行区切り)(色付き)
+    for str in ${arr_string[@]}; do
+      echo "  ${str}";
+    done
   done
   IFS=$IFS_ORG # IFS変数を復元
   exit 0;
-    
-else
+
+elif [ "$1" = "install" ]; then # installコマンド
+  sudo chmod 744 ${DIR}$(basename $0)
+  sudo chown ${userName} ${DIR}$(basename $0)
+  sudo cp ${DIR}$(basename $0) /usr/local/bin/$(basename $0)
+  exit 0;
+
+else # 存在しないコマンド
   echo "command '${1}' not found"
 fi
-
-
-
-
-# sudo mv ./md /usr/local/bin/ でPATH通っている場所に入れて
-# sudo chmod 744 /usr/local/bin/md を確認して
-# sudo chown tetraloba /usr/local/bin/md を確認
